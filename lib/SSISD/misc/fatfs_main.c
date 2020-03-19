351b57ebc1d5a0a93c9e574c1e8d36c15b31987b
Shreya Ravi <sravi2@stanford.edu>
2020-03-19 19:14:21 -0400

    add needed libraries

---
+++ lib/SSISD/misc/fatfs_main.c
@@ -0,0 +1,26 @@
+#include "fatfs_main.h"
+
+/**
+ * \brief Current time returned is packed into a DWORD value.
+ *
+ * The bit field is as follows:
+ *
+ * bit31:25  Year from 1980 (0..127)
+ *
+ * bit24:21  Month (1..12)
+ *
+ * bit20:16  Day in month(1..31)
+ *
+ * bit15:11  Hour (0..23)
+ *
+ * bit10:5   Minute (0..59)
+ *
+ * bit4:0    Second (0..59)
+ *
+ * \return Current time.
+ */
+
+DWORD get_fattime(void)
+{
+	return 0;
+}
---
+++ lib/SSISD/misc/fatfs_main.h
@@ -0,0 +1,16 @@
+#ifndef FATFS_MAIN_H
+#define FATFS_MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include "ff.h"
+
+DWORD get_fattime(void);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // FATFS_MAIN_H
